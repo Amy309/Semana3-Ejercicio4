4c2368d42ddddf8605c3104b8dd4b1fa27545f0a
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-08-17 18:16:40 -0600

    Ejercicio4

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace array
+{
+    class programs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 4\n");;
+
+            Console.WriteLine("Funcion BinarySearch\n");
+
+            string[] a = { "Sol", "Dedo", "Casa" };
+
+            System.Array.Sort(a, 0, a.Length);
+            foreach (var j in a)
+                Console.WriteLine(j);
+
+            Console.WriteLine("");
+
+            int index1 = Array.BinarySearch(a, "Sol");
+            int index2 = Array.BinarySearch(a, "Dedo");
+            int index3 = Array.BinarySearch(a, "Casa");
+
+            Console.WriteLine(index1);
+            Console.WriteLine(index2);
+            Console.WriteLine(index3);
+        }
+    }
+}
